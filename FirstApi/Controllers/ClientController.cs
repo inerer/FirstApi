3514a684059f3a4d5a70bfb435c08beebdee5408
inerer <arshinov04@gmail.com>
2022-12-11 23:49:54 +0300

    f

--- FirstApi/Controllers/ClientController.cs
+++ FirstApi/Controllers/ClientController.cs
@@ -15,27 +15,29 @@ namespace FirstApi.Controllers
     public class ClientController : ControllerBase, IClientService
     {
         private ClientService _clientService;
-
+        
         public ClientController(IConfiguration configuration)
         {
             _clientService = new ClientService(configuration);
         }
 
+        [HttpGet("GetClient")]
         public IActionResult GetClient(int id)
         {
             return _clientService.GetClient(id);
         }
-
+        [HttpPost("AddClient")]
         public IActionResult AddClient(Client client)
         {
             throw new NotImplementedException();
         }
 
+        [HttpPost("EditClient")]
         public IActionResult EditClient(Client client)
         {
             throw new NotImplementedException();
         }
-
+        [HttpPost("DeleteClient")]
         public IActionResult DeleteClient(Client client)
         {
             throw new NotImplementedException();
